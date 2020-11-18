155bef800c869dcfd7e73eeb2dfbc11da4de862c
kuunal <kunaldeshmukh2503@gmail.com>
2020-11-18 14:01:55 +0530

    [Kunal] ADD: Fucntion to generate fare

---
+++ CabInvoice/InvoiceGenerator.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace CabInvoice
+{
+    public class Program
+    {
+        const int COST_PER_KILOMETER = 10;
+        const int COST_PER_MINUTE = 1;
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+
+        public double GenerateFare(double distance, int time)
+        {
+            return COST_PER_KILOMETER * distance + time * COST_PER_MINUTE;
+        }
+    }
+}
