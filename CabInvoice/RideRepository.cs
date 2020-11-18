f044f07606436f790c5e92e67193bc0d3706c9c8
kuunal <kunaldeshmukh2503@gmail.com>
2020-11-18 18:47:16 +0530

    [Kunal] ADD: Repository containing dictionary

---
+++ CabInvoice/RideRepository.cs
@@ -0,0 +1,38 @@
+﻿using CabInvoice.Enums;
+using CabInvoice.Exceptions;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace CabInvoice
+{
+    public class RideRepository
+    {
+        //static RideRepository rideRepository = new RideRepository();
+        Dictionary<string, List<Ride>> userDict = new Dictionary<string, List<Ride>>();
+
+        //public RideRepository GetRideRepository()
+        //{
+        //    return rideRepository;
+        //}
+
+        public void AddUser(string userid, Ride[] rides)
+        {
+            if (userDict.ContainsKey(userid))
+                userDict[userid].AddRange(rides.ToList());
+            else
+                userDict.Add(userid, rides.ToList());
+        }
+
+        public Ride[] GetRides(string userid)
+        {
+            try { 
+                return userDict[userid].ToArray();
+            }catch(Exception e)
+            {
+                throw new CabInvoiceExceptions(ExceptionEnums.ExceptionType.NO_SUCH_USER);
+            }
+        }
+    }
+}
