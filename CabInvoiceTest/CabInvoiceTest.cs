33dafd5a739b0f942b9e1ffb1737a78709e61892
kuunal <kunaldeshmukh2503@gmail.com>
2020-11-18 14:48:39 +0530

    [Kunal] ADD: Test case for average fare

--- CabInvoiceTest/CabInvoiceTest.cs
+++ CabInvoiceTest/CabInvoiceTest.cs
@@ -12,17 +12,27 @@ namespace CabInvoiceTest
         }
 
         [Test]
-        public void givenDistanceAndTime_WhenToCalculateFare_ShouldReturnFare()
+        public void GivenDistanceAndTime_WhenToCalculateFare_ShouldReturnFare()
         {
             double fare = program.GenerateFare(2, 5);
             Assert.AreEqual(25, fare);
         }
 
         [Test]
-        public void givenDistanceAndTime_WhenToCalculateMinimumFare_ShouldReturnMinimumFare()
+        public void GivenDistanceAndTime_WhenToCalculateMinimumFare_ShouldReturnMinimumFare()
         {
             double fare = program.GenerateFare(0.1, 1);
             Assert.AreEqual(5, fare);
+        }
+
+        [Test]
+        public void GivenDistanceAndTime_WhenToCalculateForMultipleFare_ShouldReturnAverageFare()
+        {
+            Ride[] rides = {
+            new Ride(2.0,5),
+            new Ride(0.1,1)
+            };
+            Assert.AreEqual(15, program.GenerateAverageFare(rides));
         }
     }
 }
