ebd0b97052e6d6debff429b2a3a232f4743d524f
kuunal <kunaldeshmukh2503@gmail.com>
2020-11-18 18:41:37 +0530

    [kunal] ADD: Exceptions for cabinvoice

---
+++ CabInvoice/Exceptions/CabInvoiceExceptions.cs
@@ -0,0 +1,17 @@
+﻿using CabInvoice.Enums;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CabInvoice.Exceptions
+{
+    class CabInvoiceExceptions : Exception
+    {
+        public ExceptionEnums.ExceptionType exceptionType;
+
+        public CabInvoiceExceptions(ExceptionEnums.ExceptionType NO_SUCH_USER) : base(NO_SUCH_USER.ToString())
+        {
+            this.exceptionType = NO_SUCH_USER;
+        }
+    }
+}
