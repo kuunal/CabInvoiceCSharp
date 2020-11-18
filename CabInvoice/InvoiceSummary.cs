dabfad3fa2203693477a537ffb1481881d94cb3f
kuunal <kunaldeshmukh2503@gmail.com>
2020-11-18 15:43:20 +0530

    [Kunal] ADD: Class for Invoice summary

---
+++ CabInvoice/InvoiceSummary.cs
@@ -0,0 +1,30 @@
+﻿using CabInvoice;
+
+namespace CabInvoice
+{
+    public class InvoiceSummary
+    {
+        int totalRides;
+        double totalFare;
+        double averageFarePerRide;
+
+        public InvoiceSummary(int totalRides, double AverageFare)
+        {
+            this.totalRides = totalRides;
+            this.totalFare = AverageFare*totalRides;
+            this.averageFarePerRide = AverageFare;
+        }
+
+        public override bool Equals(object that)
+        {
+            if (this == that)
+                return true;
+            if (this == null)
+                return false;
+            InvoiceSummary obj = (InvoiceSummary)that;
+            return this.averageFarePerRide == obj.averageFarePerRide
+                && this.totalFare == obj.totalFare
+                && this.totalRides == obj.totalRides; 
+        }
+    }
+}
