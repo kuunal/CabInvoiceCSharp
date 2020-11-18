90efcb44c5f4402cc6fa111d7b8e687ace46e734
kuunal <kunaldeshmukh2503@gmail.com>
2020-11-18 14:11:52 +0530

    [Kunal] ADD: Test case for minimum fare

--- CabInvoiceTest/CabInvoiceTest.cs
+++ CabInvoiceTest/CabInvoiceTest.cs
@@ -12,10 +12,17 @@ namespace CabInvoiceTest
         }
 
         [Test]
-        public void Test1()
+        public void givenDistanceAndTime_WhenToCalculateFare_ShouldReturnFare()
         {
             double fare = program.GenerateFare(2, 5);
             Assert.AreEqual(25, fare);
+        }
+
+        [Test]
+        public void givenDistanceAndTime_WhenToCalculateMinimumFare_ShouldReturnMinimumFare()
+        {
+            double fare = program.GenerateFare(0.1, 1);
+            Assert.AreEqual(5, fare);
         }
     }
 }
